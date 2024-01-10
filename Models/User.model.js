6cf81fe3993a14884c05ecb524c23b87e0d44a29
HirushiPremarathna <119779113+HirushiPremarathna@users.noreply.github.com>
2024-01-10 16:23:06 +0530

    hashing password using bcrypt added
     hash a user password before saving it inside mongoDB database, since we are using mongoose , mongoose middleware has chosen to hash the password just before saving using presave middleware.

--- Models/User.model.js
+++ Models/User.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const UserSchema = new Schema({
   email: {
@@ -12,6 +13,29 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+});
+
+UserSchema.pre("save", async function (next) {
+  try {
+    //hash the password
+    console.log("called before saving the user")
+    const salt = await bcrypt.genSalt(10); //10 is the number of rounds
+    //create a password hash (hash + salt = password)
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next(); //go to the next middleware
+  } catch (error) {
+    next(error);
+  }
+});
+
+UserSchema.post("save", function (next) {
+  try {
+    console.log("called after saving the user");
+  } catch (error) {
+    next(error);
+    
+  }
 });
 
 const User = mongoose.model("user", UserSchema);
